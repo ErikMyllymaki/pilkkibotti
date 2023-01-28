9b657bfb1892314bf4f442a03be9ee2c70364632
ErikMyllymaki <erik.myl01@gmail.com>
2023-01-28 18:01:31 +0200

    joo

--- pp2bot.py
+++ pp2bot.py
@@ -4,12 +4,15 @@ pyautogui.PAUSE = 0.01
 
 while True:
     fishing = False
-
+    ritsa = pyautogui.locateOnScreen('./images/ritsa.png', confidence=0.6)
+    if ritsa:
+            fishing=True
+            print('ritsa pilkkivalmiina, poistutaan pilkkinäkymästä')
+            pyautogui.click(out)
     if not fishing:
         print('ei kalasteta')
         smallsurvari = pyautogui.locateOnScreen('./images/smallsurvari.png', confidence=0.9)
         emptyhook = pyautogui.locateOnScreen('./images/emptyhook.png', confidence=0.9)
-        ritsa = pyautogui.locateOnScreen('./images/ritsa.png', confidence=0.6)
         holmes = pyautogui.locateOnScreen('./images/holmes.png', confidence=0.6)
         survi = pyautogui.locateOnScreen('./images/survi.png', confidence=0.9)
         rod = pyautogui.locateOnScreen('./images/rod.png', confidence=0.6)
@@ -21,10 +24,7 @@ while True:
         if emptyhook:
                 print('tyhjä koukku, mato vaihdetaan')
                 pyautogui.click(emptyhook)
-        if ritsa:
-                fishing=True
-                print('ritsa pilkkivalmiina, poistutaan pilkkinäkymästä')
-                pyautogui.click(out)
+
 
     if fishing:
         while fishing:
